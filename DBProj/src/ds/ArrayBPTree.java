62094f4c1ccb0cd36398588c9e9293a3f8490991
fatemezahrafathian <126403410+fatemezahrafathian@users.noreply.github.com>
2024-01-29 18:13:08 +0330

    Implementation of  search and  Insertion operation

--- DBProj/src/ds/ArrayBPTree.java
+++ DBProj/src/ds/ArrayBPTree.java
@@ -741,18 +741,134 @@ class ArrayBPTree<K, V> extends AbstractBPTree<K, V> {
 
     /*~~~~~~~~~~~~~~~~ API: DELETE, INSERT, SEARCH ~~~~~~~~~~~~~~~~*/
 
+    /**
+     * Given an integer key and floating point value, this method inserts a
+     * dictionary pair accordingly into the B+ tree.
+     * @param key: an integer key to be used in the dictionary pair
+     * @param value: a floating point number to be used in the dictionary pair
+     */
     @Override
-    public Entry<K, V> insert(K key, V value) {
-        return null;
+    public Entry<K,V> insert(K key, V value){
+        if (isEmpty()) {
+
+            /* Flow of execution goes here only when first insert takes place */
+
+            // Create leaf node as first node in B plus tree (root is null)
+            Entry<K,V> entry =new LeafNode.DictionaryPair(key, value);
+            LeafNode ln = new LeafNode(this.m,entry );
+
+            // Set as first leaf node (can be used later for in-order leaf traversal)
+            this.firstLeaf = ln;
+            return entry;
+
+        } else {
+
+            // Find leaf node to insert into
+            LeafNode ln = (this.root == null) ? this.firstLeaf : findLeafNode(key);
+
+            // Insert into leaf node fails if node becomes overfull
+            Entry<K,V> entry =new LeafNode.DictionaryPair(key, value);
+            if (!ln.insert(entry)) {
+
+                // Sort all the dictionary pairs with the included pair to be inserted
+                ln.dictionary[ln.numPairs] = (LeafNode.DictionaryPair) entry;
+                ln.numPairs++;
+                sortDictionary(ln.dictionary);
+
+                // Split the sorted pairs into two halves
+                int midpoint = getMidpoint();
+                Entry<K,V>[] halfDict = splitDictionary(ln, midpoint);
+
+                if (ln.parent == null) {
+
+                    /* Flow of execution goes here when there is 1 node in tree */
+
+                    // Create internal node to serve as parent, use dictionary midpoint key
+
+                    K[] parent_keys = (K[])Array.newInstance(key.getClass(),this.m);
+                    parent_keys[0] = halfDict[0].getKey();
+                    InternalNode parent = new InternalNode(this.m, parent_keys);
+                    ln.parent = parent;
+                    parent.appendChildPointer(ln);
+
+                } else {
+
+                    /* Flow of execution goes here when parent exists */
+
+                    // Add new key to parent for proper indexing
+                    K newParentKey = halfDict[0].getKey();
+                    ln.parent.keys[ln.parent.degree - 1] = newParentKey;
+                    Arrays.sort(ln.parent.keys, 0, ln.parent.degree);
+                }
+
+                // Create new LeafNode that holds the other half
+                LeafNode newLeafNode = new LeafNode(this.m, halfDict, ln.parent);
+
+                // Update child pointers of parent node
+                int pointerIndex = ln.parent.findIndexOfPointer(ln) + 1;
+                ln.parent.insertChildPointer(newLeafNode, pointerIndex);
+
+                // Make leaf nodes siblings of one another
+                newLeafNode.rightSibling = ln.rightSibling;
+                if (newLeafNode.rightSibling != null) {
+                    newLeafNode.rightSibling.leftSibling = newLeafNode;
+                }
+                ln.rightSibling = newLeafNode;
+                newLeafNode.leftSibling = ln;
+
+                if (this.root == null) {
+
+                    // Set the root of B+ tree to be the parent
+                    this.root = ln.parent;
+
+                } else {
+
+					/* If parent is overfull, repeat the process up the tree,
+			   		   until no deficiencies are found */
+                    InternalNode in = ln.parent;
+                    while (in != null) {
+                        if (in.isOverfull()) {
+                            splitInternalNode(in);
+                        } else {
+                            break;
+                        }
+                        in = in.parent;
+                    }
+                }
+            }
+            return entry;
+        }
+    }
+
+    /**
+     * Given a key, this method returns the value associated with the key
+     * within a dictionary pair that exists inside the B+ tree.
+     * @param key: the key to be searched within the B+ tree
+     * @return the floating point value associated with the key within the B+ tree
+     */
+    @Override
+    public V search(K key) {
+
+        // If B+ tree is completely empty, simply return null
+        if (isEmpty()) { return null; }
+
+        // Find leaf node that holds the dictionary key
+        LeafNode ln = (this.root == null) ? this.firstLeaf : findLeafNode(key);
+
+        // Perform binary search to find index of key within dictionary
+        Entry<K,V>[] dps = ln.dictionary;
+        int index = binarySearch(dps, ln.numPairs, key);
+
+        // If index negative, the key doesn't exist in B+ tree
+        if (index < 0) {
+            return null;
+        } else {
+            return dps[index].getValue();
+        }
     }
 
     @Override
     public Entry<K, V> delete(K key) {
-        return null;
-    }
-
-    @Override
-    public V search(K key) {
         return null;
     }
 
